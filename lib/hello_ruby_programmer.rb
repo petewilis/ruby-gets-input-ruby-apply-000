9d5daf73ea2961f01a156bcaec82f98ddff65f06
Jonathan Mattos <jonmattos2014@gmail.com>
2020-11-01 23:21:51 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,3 @@
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
-end
+endhttps://dl.dropboxusercontent.com/s/tijh1wyuvdfz11a/2015-09-10%20at%2012.13%20PM.png
